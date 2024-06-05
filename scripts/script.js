4319ef4b84ea25e29ae76bc2367554b15c3d6052
ItsDiegoTBG <ItsDiegoTBG>
2024-06-04 21:36:56 -0500

    Ahora si ultimos cambios

---
+++ scripts/script.js
@@ -0,0 +1,156 @@
+var LLP= document.getElementById("LLP");
+LLP.addEventListener('mouseover', function() {
+    LLP.querySelectorAll('li').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    LLP.querySelectorAll('h3').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    LLP.querySelectorAll('ul').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    this.style.backgroundColor =  "#272343"; // Cambia a azul al pasar el cursor
+    this.style.color = "#fffffe";
+});
+LLP.addEventListener('mouseout', function() {
+    LLP.querySelectorAll('li').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    LLP.querySelectorAll('h3').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    LLP.querySelectorAll('ul').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    this.style.backgroundColor = 'rgb(169, 219, 218)'; // Restablece al color original
+    this.style.color = "#272343";
+});
+var LBD= document.getElementById("LBD");
+LBD.addEventListener('mouseover', function() {
+    LBD.querySelectorAll('li').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    LBD.querySelectorAll('h3').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    LBD.querySelectorAll('ul').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    this.style.backgroundColor =  "#272343"; // Cambia a azul al pasar el cursor
+    this.style.color = "#fffffe";
+});
+LBD.addEventListener('mouseout', function() {
+    LBD.querySelectorAll('li').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    LBD.querySelectorAll('h3').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    LBD.querySelectorAll('ul').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    this.style.backgroundColor = 'rgb(169, 219, 218)'; // Restablece al color original
+    this.style.color = "#272343";
+});
+
+var LHD= document.getElementById("LHD");
+LHD.addEventListener('mouseover', function() {
+    LHD.querySelectorAll('li').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    LHD.querySelectorAll('h3').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    LHD.querySelectorAll('ul').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    this.style.backgroundColor =  "#272343"; // Cambia a azul al pasar el cursor
+    this.style.color = "#fffffe";
+});
+LHD.addEventListener('mouseout', function() {
+    LHD.querySelectorAll('li').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    LHD.querySelectorAll('h3').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    LHD.querySelectorAll('ul').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    this.style.backgroundColor = 'rgb(169, 219, 218)'; // Restablece al color original
+    this.style.color = "#272343";
+});
+
+var LOT= document.getElementById("LOT");
+LOT.addEventListener('mouseover', function() {
+    LOT.querySelectorAll('li').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    LOT.querySelectorAll('h3').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    LOT.querySelectorAll('ul').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    this.style.backgroundColor =  "#272343"; // Cambia a azul al pasar el cursor
+    this.style.color = "#fffffe";
+});
+LOT.addEventListener('mouseout', function() {
+    LOT.querySelectorAll('li').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    LOT.querySelectorAll('h3').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    LOT.querySelectorAll('ul').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    this.style.backgroundColor = 'rgb(169, 219, 218)'; // Restablece al color original
+    this.style.color = "#272343";
+});
+
+var nav= document.getElementById("Navegacion");
+nav.addEventListener('mouseover', function() {
+    nav.querySelectorAll('lo').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+    nav.querySelectorAll('a').forEach(item => {
+        item.style.backgroundColor = "#272343";
+        item.style.color = "#fffffe";
+    }); 
+});
+nav.addEventListener('mouseout', function() {
+    nav.querySelectorAll('lo').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+    nav.querySelectorAll('a').forEach(item => {
+        item.style.backgroundColor = 'rgb(169, 219, 218)';
+        item.style.color = "#272343";
+    }); 
+});
